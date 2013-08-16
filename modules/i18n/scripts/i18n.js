5ede58b2a90950e59eaff26e691c9e791410d79c
madhuka <madhukaudantha@gmail.com>
2013-08-16 14:30:23 +0530

    updating i18n

--- modules/i18n/scripts/i18n.js
+++ modules/i18n/scripts/i18n.js
@@ -1,24 +1,39 @@
+
 (function () {
     var localeResourcesBasePath = '',
-        localizations = {};
+            localizations = {};
+    fallBackLocalizations = {};
     //initialization with the request and loads the correct localization resources
-    this.init = function (req) {
+    this.init = function (req, localeResourcePath) {
         var locale = req.getLocale();
         try {
-            localizations = require(localeResourcesBasePath + 'locale_' + locale + '.json');
+            if (localeResourcePath) {
+                localeResourcesBasePath = localeResourcePath;
+            }
+            var file = new File(localeResourcesBasePath + 'locale_' + locale + '.json');
+            //Check relevant locale file exists
+            if (file.isExists()) {
+                localizations = require(localeResourcesBasePath + 'locale_' + locale + '.json');
+                fallBackLocalizations = require(localeResourcesBasePath + 'locale_default.json');
+            } else {  //If not reading Strings from default English locale file.
+                localizations = require(localeResourcesBasePath + 'locale_en.json');
+            }
         } catch (e) {
             localizations = {};
         }
     };
 
-    var getLocalString = function (key, fallback) {
+    var getLocalString = function (key) {
         if (localizations[key]) {
             return localizations[key]
+        } else if(fallBackLocalizations) {
+            return fallBackLocalizations[key];
         } else {
-            return  key;
+            return key;
         }
 
     };
+
 
     this.localize = function (key, fallback) {
         var localized = getLocalString(key);
