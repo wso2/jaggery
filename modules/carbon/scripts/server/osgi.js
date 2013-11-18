4c77e4e24edbea6964aa4b971cd4cd8ea7d11f83
ruchiraw <ruchira.wageesha@gmail.com>
2013-11-18 13:10:26 +0530

    fixing issues with thread local context

--- modules/carbon/scripts/server/osgi.js
+++ modules/carbon/scripts/server/osgi.js
@@ -2,10 +2,9 @@ var server = {};
 
 (function (server) {
     var PrivilegedCarbonContext = Packages.org.wso2.carbon.context.PrivilegedCarbonContext,
-        context = PrivilegedCarbonContext.getThreadLocalCarbonContext(),
         Class = java.lang.Class;
 
     server.osgiService = function (clazz) {
-        return context.getOSGiService(Class.forName(clazz));
+        return PrivilegedCarbonContext.getThreadLocalCarbonContext().getOSGiService(Class.forName(clazz));
     };
 }(server));
