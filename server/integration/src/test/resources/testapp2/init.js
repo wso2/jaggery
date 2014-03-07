a4c07fd2c95e0ccdee6b63903f4d24039de97f07
madhuka <madhuka@wso2.com>
2014-03-07 16:53:00 +0530

    upgrading application.server() test

--- server/integration/src/test/resources/testapp2/init.js
+++ server/integration/src/test/resources/testapp2/init.js
@@ -1,21 +1,59 @@
 var log = new Log();
 log.info("Initializing Jaggery test app2");
 application.serve(function test(request, respond, session) {
-    //log.info("jaggery Application Serve is ping");     
-    if (action == "query") {
-        print(request.getQueryString());
-    } else if (action == "url") {
-        print(request.getRequestURI());
-    } else if (action == "action") {
-        print(request.getParameter("action"));
-    } else if (action == "session") {
-        Session["wso2"] = "testing";
-        print("session testing. ");
-        var name = Session["wso2"];
-        print("Data is " + name);
-    } else {
-        print("application serve");
-    }
+    var action = request.getParameter("action");
+    log.debug("jaggery Application Serve is ping");
 
+    switch(action) {
+        case "query":
+            print(request.getQueryString());
+            break;
+
+        case "url" :
+            print(request.getQueryString());
+            break;
+
+        case "secure" :
+            print(request.isSecure());
+            break;
+
+        case "port" :
+            print(request.getLocalPort());
+            break;
+
+        case "response" :
+            response.content = "My response content";
+            break;
+
+        case "responseJson" :
+            response.content = {
+                products : ["Jaggery", "ESB"]
+            };
+            break;
+
+        case "sessionset" :
+            session.set("wso2", "testing");
+            print("session testing. ");
+            break;
+
+        case "sessionget" :
+            var name = session.get("wso2");
+            print(name);
+            break;
+            
+        //checking session     
+        case "session-set" :
+            Session["wso2"] = "testing";
+            print("session testing. ");
+            break;
+
+        case "session-get" :
+            var name = Session["wso2"];
+            print(name);
+            break;
+
+        default:
+            print('app serve testing');
+    }
 
 });
