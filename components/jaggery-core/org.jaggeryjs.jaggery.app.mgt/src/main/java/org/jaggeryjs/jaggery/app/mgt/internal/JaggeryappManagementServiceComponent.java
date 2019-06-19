ff6e993b9e017a76ae0811628a5578a5ae2d7845
senthalan <senthalank@gmail.com>
2019-06-19 15:50:54 +0530

    Change the scr doc comment to component annotation

--- components/jaggery-core/org.jaggeryjs.jaggery.app.mgt/src/main/java/org/jaggeryjs/jaggery/app/mgt/internal/JaggeryappManagementServiceComponent.java
+++ components/jaggery-core/org.jaggeryjs.jaggery.app.mgt/src/main/java/org/jaggeryjs/jaggery/app/mgt/internal/JaggeryappManagementServiceComponent.java
@@ -18,69 +18,88 @@ package org.jaggeryjs.jaggery.app.mgt.internal;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.wso2.carbon.registry.core.service.RegistryService;
 import org.wso2.carbon.user.core.service.RealmService;
 import org.wso2.carbon.utils.ConfigurationContextService;
-//import org.wso2.carbon.tomcat.ext.valves.CarbonTomcatValve;
-import org.wso2.carbon.webapp.mgt.GhostWebappDeployerValve;
-import org.wso2.carbon.webapp.mgt.TenantLazyLoaderValve;
-//import org.wso2.carbon.tomcat.ext.valves.TomcatValveContainer;
 import org.wso2.carbon.webapp.mgt.DataHolder;
 
-import java.util.ArrayList;
+// import org.wso2.carbon.tomcat.ext.valves.CarbonTomcatValve;
+// import org.wso2.carbon.tomcat.ext.valves.TomcatValveContainer;
 
-/**
- * @scr.component name="org.jaggeryjs.jaggery.app.mgt.internal.JaggeryappManagementServiceComponent"
- * immediate="true"
- * @scr.reference name="config.context.service"
- * interface="org.wso2.carbon.utils.ConfigurationContextService"
- * cardinality="1..1" policy="dynamic"  bind="setConfigurationContextService"
- * unbind="unsetConfigurationContextService"
- * @scr.reference name="user.realmservice.default" interface="org.wso2.carbon.user.core.service.RealmService"
- * cardinality="1..1" policy="dynamic" bind="setRealmService"  unbind="unsetRealmService"
- * @scr.reference name="registry.service" interface="org.wso2.carbon.registry.core.service.RegistryService"
- * cardinality="1..1" policy="dynamic"  bind="setRegistryService" unbind="unsetRegistryService"
- */
+@Component(
+        name = "org.jaggeryjs.jaggery.app.mgt.internal.JaggeryappManagementServiceComponent",
+        immediate = true)
 public class JaggeryappManagementServiceComponent {
+
     private static final Log log = LogFactory.getLog(JaggeryappManagementServiceComponent.class);
 
-
+    @Activate
     protected void activate(ComponentContext ctx) {
+
         try {
             // Register the valves with Tomcat
-            //ArrayList<CarbonTomcatValve> valves = new ArrayList<CarbonTomcatValve>();
-            //valves.add(new TenantLazyLoaderValve());
-            //valves.add(new GhostWebappDeployerValve());
-            //TomcatValveContainer.addValves(valves);
+            // ArrayList<CarbonTomcatValve> valves = new ArrayList<CarbonTomcatValve>();
+            // valves.add(new TenantLazyLoaderValve());
+            // valves.add(new GhostWebappDeployerValve());
+            // TomcatValveContainer.addValves(valves);
         } catch (Exception e) {
             log.error("Error occurred while activating JaggeryappManagementServiceComponent", e);
         }
     }
 
+    @Deactivate
     protected void deactivate(ComponentContext ctx) {
-//         TomcatValveContainer.removeValves();
+        // TomcatValveContainer.removeValves();
     }
 
+    @Reference(
+            name = "config.context.service",
+            service = org.wso2.carbon.utils.ConfigurationContextService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetConfigurationContextService")
     protected void setConfigurationContextService(ConfigurationContextService contextService) {
+
         DataHolder.setServerConfigContext(contextService.getServerConfigContext());
     }
 
     protected void unsetConfigurationContextService(ConfigurationContextService contextService) {
+
         DataHolder.setServerConfigContext(null);
     }
 
-
+    @Reference(
+            name = "user.realmservice.default",
+            service = org.wso2.carbon.user.core.service.RealmService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRealmService")
     protected void setRealmService(RealmService realmService) {
-        //keeping the realm service in the DataHolder class
+        // keeping the realm service in the DataHolder class
         DataHolder.setRealmService(realmService);
     }
 
     protected void unsetRealmService(RealmService realmService) {
+
     }
 
+    @Reference(
+            name = "registry.service",
+            service = org.wso2.carbon.registry.core.service.RegistryService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRegistryService")
     protected void setRegistryService(RegistryService registryService) {
+
     }
 
     protected void unsetRegistryService(RegistryService registryService) {
+
     }
 }
